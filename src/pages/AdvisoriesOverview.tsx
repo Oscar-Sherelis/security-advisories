95905ba7826a381b1aac2f0882efa9868b160c4e
Oscar <oskaras34@gmail.com>
2025-10-05 16:14:47 +0300

    Fixed import type

--- src/pages/AdvisoriesOverview.tsx
+++ src/pages/AdvisoriesOverview.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Advisory } from '../types/Advisory';
+import type { Advisory } from '../types/Advisory';
 import { fetchAdvisories } from '../services/githubApi';
 import { AdvisoryCard } from '../components/AdvisoryCard';
 
